2d0984ad2b5144520e3e5dec2b4be8f082cdcb08
adampawelka <adampawelka04@gmail.com>
2026-04-21 22:47:23 +0200

    add requireAuth middleware

---
+++ server/src/middleware/requireAuth.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express'
+import { auth } from '../auth'
+import { fromNodeHeaders } from 'better-auth/node'
+
+export const requeireAuth = async (req: Request, res: Response, next: NextFunction) => {
+    const session = await auth.api.getSession({
+        headers: fromNodeHeaders(req.headers)
+    })
+
+    if (!session) {
+        res.status(401).json({ error: 'Unauthorized' })
+        return
+    }
+
+    req.user = session.user
+    next()
+}
